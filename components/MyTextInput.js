9e8e0174cd037d0d162f1a98665a57ed4034c05d
Malcolm <sciand6@gmail.com>
2020-12-13 08:47:31 -0500

    Configured input validation.

--- components/MyTextInput.js
+++ components/MyTextInput.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { TextInput, View, Text } from "react-native";
+import { TextInput, View, Text, StyleSheet } from "react-native";
 
 export default function MyTextInput(props) {
-  const { input, meta, ...inputProps } = props;
+  const {
+    input,
+    meta: { touched, error },
+    ...inputProps
+  } = props;
 
   return (
     <View>
@@ -13,6 +17,20 @@ export default function MyTextInput(props) {
         onFocus={input.onFocus}
         value={input.value}
       />
+      {touched && error ? (
+        <Text style={styles.errorText}>{error}</Text>
+      ) : (
+        <View></View>
+      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  errorText: {
+    color: "red",
+    fontSize: 14,
+    paddingHorizontal: 16,
+    paddingBottom: 8,
+  },
+});
--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -24,11 +24,13 @@ function MyForm(props) {
         style={styles.input}
         name={"password"}
         placeholder="Password"
+        secureTextEntry={true}
         component={MyTextInput}
       />
       <Field
         style={styles.input}
         name={"passwordConfirmation"}
+        secureTextEntry={true}
         placeholder="Password Confirmation"
         component={MyTextInput}
       />
@@ -77,6 +79,24 @@ const styles = StyleSheet.create({
   },
 });
 
+const validate = (values) => {
+  const errors = {};
+  if (!values.username) {
+    errors.username = "Username is required.";
+  }
+  if (!values.email) {
+    errors.email = "Email is required.";
+  }
+  if (!values.password) {
+    errors.password = "Password is required.";
+  }
+  if (!values.password) {
+    errors.passwordConfirmation = "Passwords do not match.";
+  }
+  return errors;
+};
+
 export default reduxForm({
   form: "SignUp",
+  validate,
 })(MyForm);
